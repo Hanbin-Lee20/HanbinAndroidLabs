edfb83a17d8aeff68c7686e6d1f74789e61e8612
Hanbin Lee <lee00852@algonquinlive.com>
2023-09-22 01:28:13 -0400

    Finished week 2

--- app/src/main/java/data/MainViewModel.java
+++ app/src/main/java/data/MainViewModel.java
@@ -1,10 +1,19 @@
 package data;
 
+import android.widget.CompoundButton;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-public class MainViewModel extends ViewModel{
+public class MainViewModel extends ViewModel {
 
-//    public String editString;
     public MutableLiveData<String> editString = new MutableLiveData<>();
 
-}
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
+
+    public MutableLiveData<CompoundButton.OnCheckedChangeListener> checkedChangeListener = new MutableLiveData<>();
+
+
+
+    }
+
+
--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.Clock;
 
 import Algonquin.cst2355.lee.databinding.ActivityMainBinding;
 import data.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton imageButton;
     private MainViewModel model;
+    private MainViewModel checkedChangeListener;
     private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
-
-//        variableBinding.textview.setText(model.editString);
 
         variableBinding.mybutton.setOnClickListener(click ->
         {
@@ -41,27 +45,69 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        model.isSelected.observe(this, selected -> {
+            variableBinding.checkbox.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+            variableBinding.radio.setChecked(selected);
 
-        //Inside the method onCreate
-//        TextView mytext = variableBinding.textview;
-//        Button btn = variableBinding.mybutton;
-//        EditText myedit = variableBinding.myedittext;
+
+            CharSequence text = "The value is now:" + selected;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(this , text, duration);
+            toast.show();
+        });
+
+        variableBinding.checkbox.setOnCheckedChangeListener( (checkbox, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        } );
+
+        variableBinding.switch1.setOnCheckedChangeListener( (switch1, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        } );
+
+        variableBinding.radio.setOnCheckedChangeListener( (radio, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        } );
+
+
+//        variableBinding.myimg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Context context = getApplicationContext();
+//                CharSequence text = "You just clicked image!";
+//                int duration = Toast.LENGTH_SHORT;
 //
-//        String editString = myedit.getText().toString();
-//        btn.setOnClickListener(   (View v) ->  mytext.setText("Your edit text has: " + editString)    );
+//                Toast toast = Toast.makeText(context, text, duration);
+//                toast.show();
+//            }
+//        });
 
+        variableBinding.myimagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                int width = view.getWidth();
+                int height = view.getHeight();
 
+                Context context = getApplicationContext();
+                CharSequence text = "The width = " + width + " and height = " + height;
+                int duration = Toast.LENGTH_SHORT;
 
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
 
-
+            }
+        });
 
 
     }
 
 
+    };
 
-}
+
+
 
 
 
