bd2d3cd753704e78ff5602533dc031d8e0625e0f
Hanbin Lee <lee00852@algonquinlive.com>
2023-09-27 15:15:58 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -5,18 +5,47 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import Algonquin.cst2355.lee.R;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        imgView = findViewById(R.id.flagView);
+        sw = findViewById(R.id.switch1);
 
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
 
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+            
+        });
 
     }
 
