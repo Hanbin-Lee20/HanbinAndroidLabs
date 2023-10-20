bb32251a3dbe568e5e77a0ca5593819c382dc066
Hanbin Lee <lee00852@algonquinlive.com>
2023-10-20 19:34:12 -0400

    5 tests implemented

--- app/src/androidTest/java/ui/MainActivityTest.java
+++ app/src/androidTest/java/ui/MainActivityTest.java
@@ -39,8 +39,11 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
+    /**
+     * Numeric missing
+     */
     @Test
-    public void mainActivityTest() {
+    public void mainActivityTest1() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -51,7 +54,7 @@ public class MainActivityTest {
         }
 
         ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
-        appCompatEditText.perform(replaceText("12345"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("Aa!"), closeSoftKeyboard());
 
         ViewInteraction materialButton = onView(withId(R.id.loginButton));
         materialButton.perform(click());
@@ -60,82 +63,94 @@ public class MainActivityTest {
         textView.check(matches(withText("You shall not pass!")));
     }
 
+    /**
+     * Uppercase missing
+     */
+    @Test
+    public void mainActivityTest2() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
+        appCompatEditText.perform(replaceText("123a!"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(withId(R.id.loginButton));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * Lowercase missing
+     */
+    @Test
+    public void mainActivityTest3() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
+        appCompatEditText.perform(replaceText("123A!"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(withId(R.id.loginButton));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * Special missing
+     */
+    @Test
+    public void mainActivityTest4() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
+        appCompatEditText.perform(replaceText("123Aa"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(withId(R.id.loginButton));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * has all requirements
+     */
     @Test
     public void testFindMissingUpperCase() {
 
         ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
-        appCompatEditText.perform(replaceText("password123#$*"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("Password123#$*"), closeSoftKeyboard());
 
         ViewInteraction materialButton = onView(withId(R.id.loginButton));
         materialButton.perform(click());
 
         ViewInteraction textView = onView(withId(R.id.textView));
-        textView.check(matches(withText("You shall not pass!")));
+        textView.check(matches(withText("Your password meets the requirements")));
     }
-//
-//    @Test
-//    public void mainActivityTest3() {
-//        // Added a sleep statement to match the app's execution delay.
-//        // The recommended way to handle such scenarios is to use Espresso idling resources:
-//        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-//        try {
-//            Thread.sleep(500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
-//        appCompatEditText.perform(replaceText("1huhu*&^2345"), closeSoftKeyboard());
-//
-//        ViewInteraction materialButton = onView(withId(R.id.loginButton));
-//        materialButton.perform(click());
-//
-//        ViewInteraction textView = onView(withId(R.id.textView));
-//        textView.check(matches(withText("You shall not pass!")));
-//    }
-//
-//    @Test
-//    public void mainActivityTest4() {
-//        // Added a sleep statement to match the app's execution delay.
-//        // The recommended way to handle such scenarios is to use Espresso idling resources:
-//        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-//        try {
-//            Thread.sleep(500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
-//        appCompatEditText.perform(replaceText("123gyugyJHHI45"), closeSoftKeyboard());
-//
-//        ViewInteraction materialButton = onView(withId(R.id.loginButton));
-//        materialButton.perform(click());
-//
-//        ViewInteraction textView = onView(withId(R.id.textView));
-//        textView.check(matches(withText("You shall not pass!")));
-//    }
-//
-//    @Test
-//    public void mainActivityTest5() {
-//        // Added a sleep statement to match the app's execution delay.
-//        // The recommended way to handle such scenarios is to use Espresso idling resources:
-//        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-//        try {
-//            Thread.sleep(500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        ViewInteraction appCompatEditText = onView(withId(R.id.loginText));
-//        appCompatEditText.perform(replaceText("12cgdgdDGEGEG$%345"), closeSoftKeyboard());
-//
-//        ViewInteraction materialButton = onView(withId(R.id.loginButton));
-//        materialButton.perform(click());
-//
-//        ViewInteraction textView = onView(withId(R.id.textView));
-//        textView.check(matches(withText("Your password meets the requirements")));
-//    }
+
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
